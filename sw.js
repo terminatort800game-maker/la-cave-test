25dfc49f41e2d65a2bb613568925722505f66820
terminatort800game-maker <terminatort800game@gmail.com>
2026-03-21 12:29:18 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'la-cave-v1';
+const FILES = ['/la-cave/', '/la-cave/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
